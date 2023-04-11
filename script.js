b061be1556ba9741ef50d3796abd6e7e9a4b9456
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-04-11 19:30:59 +0400

    "feat:added point scoring logic"

--- script.js
+++ script.js
@@ -1,38 +1,64 @@
-const guessInput = document.querySelector(".guess")
-const score = document.querySelector(".score")
-const highscore = document.querySelector(".label-highscore")
-const correctNumber = document.querySelector(".number")
-const againBtn = document.querySelector(".again")
-const checkBtn = document.querySelector(".check")
-const texts = document.querySelector(".message")
-const bodyCorrect = document.getElementById("body")
+const guessInput = document.querySelector(".guess");
+let score = document.querySelector(".score");
+let highscore = document.querySelector(".label-highscore");
+const correctNumber = document.querySelector(".number");
+const againBtn = document.querySelector(".again");
+const checkBtn = document.querySelector(".check");
+const texts = document.querySelector(".message");
+const bodyCorrect = document.getElementById("body");
 
-guessInput.oninput = (e) => e.target.value = e.target.value.replace("-", "");
+let currentScore = 0;
 
-guessInput.addEventListener("input", function(){
-if (parseFloat(guessInput.value) >100){
-    guessInput.value=100
+
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
   }
-})
+  
 
-correctNumber.value = Math.floor(Math.random() * 100) + 1;
+correctNumber.value = generateRandomNumber()
 
+guessInput.oninput = (e) => (e.target.value = e.target.value.replace("-", ""));
 
+guessInput.addEventListener("input", function () {
+  if (parseFloat(guessInput.value) > 100) {
+    guessInput.value = 100;
+  }
+});
 
-checkBtn.addEventListener("click", function(){
-    if(guessInput.value == correctNumber.value){
-        document.getElementById("body").style.backgroundColor= "green"
-        texts.innerHTML = "🎉 Correct number!"
-      
-    }else if(guessInput.value < correctNumber.value && guessInput.value > 0){
-        texts.innerHTML = "📉 Too low!"
-        document.getElementById("body").style.backgroundColor= ""
-    }else if(guessInput.value > correctNumber.value){
-        document.getElementById("body").style.backgroundColor= ""
-        texts.innerHTML = "📈 Too high!"
-    }else if(guessInput.value  =" "){
-        document.getElementById("body").style.backgroundColor= ""
-        texts.innerHTML = "⛔ No number!"
-    }
-})
-console.log(correctNumber.value)
+checkBtn.addEventListener("click", function () {
+  if (guessInput.value == correctNumber.value) {
+    document.getElementById("body").style.backgroundColor = "green";
+    texts.innerHTML = "🎉 Correct number!";
+    currentScore++;
+    score.innerHTML = currentScore;
+    highscore.innerHTML = "🥇 Highscore: " + score.innerHTML
+  } else if (guessInput.value < correctNumber.value && guessInput.value > 0) {
+    texts.innerHTML = "📉 Too low!";
+    document.getElementById("body").style.backgroundColor = "";
+    currentScore++;
+    score.innerHTML = currentScore;
+  } else if (guessInput.value > correctNumber.value) {
+    document.getElementById("body").style.backgroundColor = "";
+    texts.innerHTML = "📈 Too high!";
+    currentScore++;
+    score.innerHTML = currentScore;
+  } else if (guessInput.value = " ") {
+    document.getElementById("body").style.backgroundColor = "";
+    texts.innerHTML = "⛔ No number!";
+    currentScore++;
+    score.innerHTML = currentScore;
+  }
+});
+
+againBtn.addEventListener("click", function () {
+  document.getElementById("body").style.backgroundColor = "#222";
+  texts.innerHTML = "Start guessing...";
+  guessInput.value = "";
+  currentScore = 0;
+  score.innerHTML = currentScore;
+  correctNumber.value = generateRandomNumber();
+//   console.log(correctNumber.value)
+});
+
+// console.log(correctNumber.value);
